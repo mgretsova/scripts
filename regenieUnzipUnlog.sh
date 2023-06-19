a024518b6ca348b72524c79e4300adffaa478165
mgretsova <136709001+mgretsova@users.noreply.github.com>
2023-06-19 09:53:02 +0200

    decompress -f Update regenieUnzipUnlog.sh

--- regenieUnzipUnlog.sh
+++ regenieUnzipUnlog.sh
@@ -7,6 +7,6 @@ NAME1=$(basename ${file:0:-3})
 NAME2=${DOUTPUT}'/'${NAME1}'.unloged'
 echo ${NAME1}
 echo ${NAME2}
-gzip -d ${regenie}
+gzip -d -f ${regenie}
 awk 'BEGIN{FS=" "} {if (NR>1) {$12=(10**((-1)*$12)); print $0} else {$12="PVAL"; print $0}}' ${NAME1} > ${NAME2}
 done
